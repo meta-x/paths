afa7a8990bdc2c43a8fc7c7413c8226b8a702709
ttasterisco <ttasterisco@gmail.com>
2014-05-14 17:59:06 -0700

    start

---
+++ README.md
@@ -0,0 +1,27 @@
+# paths
+
+Enhanced routing library for Clojure/Ring.
+
+
+
+## Example
+
+
+
+## Installation
+
+
+
+## Usage
+
+
+
+## Notes
+
+
+
+## License
+
+Copyright © 2014 Tony Tam
+
+Released under the MIT license.
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject paths "0.1.0-beta1"
+  :description "Enhanced routing library for Clojure/Ring."
+  :url "https://github.com/meta-x/paths"
+  :license {
+    :name "The MIT License"
+    :url "http://opensource.org/licenses/MIT"
+  }
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+  ]
+  :deploy-repositories [
+    ["clojars" {:sign-releases false}]
+  ]
+)
