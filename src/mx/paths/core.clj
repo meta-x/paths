a224ac1dd9d8d66deb96b2c5f4f2fc7e0708ea39
ttasterisco <ttasterisco@gmail.com>
2014-05-19 18:14:28 -0700

    removed some redudant checks started routing impl - finds the correct handler for a given http request

--- src/mx/paths/core.clj
+++ src/mx/paths/core.clj
@@ -6,19 +6,19 @@
 (def PATH_DELIMITER_DISCARD #"/")
 
 (defn tokenize-path [path]
-  (clojure.string/split path PATH_DELIMITER_DISCARD))
+  (clojure.string/split path PATH_DELIMITER_KEEP))
 
 (defn- last-token? [tokens]
   (= (count tokens) 1))
 
-(defn create-branch [tree tokens actions]
-  (if-let [t (first tokens)]
+(defn- create-branch [tree tokens actions]
+  (let [t (first tokens)]
     (let [r-t (get tree t {})]
       (if (last-token? tokens)
         (->> (merge r-t actions)
              (assoc tree t))
         (assoc tree t {:subroutes (create-branch tree (rest tokens) actions)})))
-    tree))
+    ))
 
 (defn- get-next-route [[path actions]]
   (if (nil? path)
@@ -29,32 +29,48 @@
   (loop [r routes
          tree {}]
     (if-let [[path actions] (get-next-route r)]
-      (do
-        (println path)
-        (let [path-tokens (tokenize-path path)
-              new-branch (create-branch {} path-tokens actions)
-              new-tree (combine tree new-branch)]
-          (println new-tree)
-          (println "----------------------------------")
-          (recur (drop 2 r) new-tree)))
+      (let [path-tokens (tokenize-path path)
+            new-branch (create-branch {} path-tokens actions)
+            new-tree (combine tree new-branch)]
+        (recur (drop 2 r) new-tree))
       tree))
   )
 
 
-(defn route [request routes]
+
+(defn find-path [tree tokens]
+  (let [t (first tokens)]
+    (if (last-token? tokens)
+      ; TODO implement logic to treat :id/:etc elements as wildcards
+      ; if -not-found-, find a key that starts with wildcard element ':''
+      ; and use it. in other frameworks/routing libs, this value is passed as an argument
+      ; to the handler function
+      (get tree t) ; returns the leaf nodes (aka the actions map)
+      (find-path (:subroutes (get tree t)) (rest tokens)))
+    ))
+
+(defn route [request routes-tree]
   ; route the request to the correct handler
   (let [path (:uri request)
-        routes (:routes route)]
-
+        method (:request-method request)
+        path-tokens (tokenize-path path)]
+    (-> (find-path routes-tree path-tokens)
+        (get method))
     ))
 
 
 
-(defn router [route-def]
+(defn router [routes-def]
   "takes a route definition, ''compiles it'' and returns a ring handler function
   that will route requests to the correct endpoint handler"
-  (fn [request]
-    ; todo: (route request routes)
-    )
-  )
-
+  (let [tree (create-tree routes-def)]
+    (fn [request]
+      ; TODO: call the handler function
+      ; if it's a simple defn, pass the request
+      ; if it's a defhandler, unwrap arguments from :params and pass them in the correct order
+      ; e.g. defined by the defhandler macro
+      ; (let [handler (route request tree)]
+      ;   (handler request)
+      ; )
+      (route request tree)
+      )))
