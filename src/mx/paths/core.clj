f036d96e7c4e8076f8792c1ed8706f7848e911bb
ttasterisco <ttasterisco@gmail.com>
2014-06-11 21:38:25 -0700

    make file-response/resource-response root folders explicit

--- src/mx/paths/core.clj
+++ src/mx/paths/core.clj
@@ -12,7 +12,6 @@
   resource-handler file-handler)
 
 ; TODO:
-; - doesn't support file download yet!
 ; - prune the tree from the delimiter (PATH-DELIMITER-KEEP keeps "/") - implement my own tokenizer code? ugh
 ; - should really implement the feature where I can send the request object with other parameters
 ; - check https://github.com/ztellman/automat for faster tokenization
@@ -172,8 +171,8 @@
 
 ;;; resource and file handling
 
-(defn- handle-resource
-  "The default resource handler."
+(defn handle-resource
+  "The default handler for resource requests."
   [request]
   (->
     request
@@ -188,19 +187,20 @@
   Add this \"/my-resource-path/:*\" resource-handler to your `paths` definition."
   {:any #'handle-resource})
 
-(defn- handle-file
-  ""
+(defn handle-file
+  "The default handler for static file requests."
   [request]
   (->
     request
     (:uri)
-    (file-response)
+    (file-response {:root "resources/public"})
     (content-type-response request)
     (not-modified-response request)
     (head-response request)))
 
 (def file-handler
-  ""
+  "Quick helper to be used in static file route definition.
+  Add this \"/my-file-path/:*\" file-handler to your `paths` definition."
   {:any #'handle-file})
 
 ;;; router handler
