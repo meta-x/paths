a4b336c9aa8834e4a034ebe63bded5a764b3b59f
ttasterisco <ttasterisco@gmail.com>
2014-08-10 02:37:26 -0700

    added basic tests for matcher and dispatcher

--- test/paths/test/dispatcher.clj
+++ test/paths/test/dispatcher.clj
@@ -1,21 +1,27 @@
 (ns paths.test.dispatcher
   (:require [clojure.test :refer [deftest testing is]]
+            [mx.paths.tree :refer [create-routes-tree]]
             [mx.paths.dispatcher :refer :all]))
 
-; dispatcher.clj
-; (dispatch routes-tree request)
+(defn h1 []
+  {:body :h1})
+(defn h2 [arg]
+  {:body {:h2 arg}})
+(defn h3 []
+  {:body :h3})
+(defn four [request]
+  {:body :404})
+
+(def routes [
+  "/" {:get #'h1}
+  "/hello" {:get #'h2}
+  "/hello/world" {:get #'h3}
+  :404 {:any #'four}
+])
+
 (deftest dispatcher
-  )
-
-; (defn x1 []
-;   (println "slash"))
-; (defn four []
-;   (println "404!"))
-
-; (def routes [
-;   "/" {:get #'x1}
-; ])
-
-; (def rt (t/create-routes-tree routes))
-; (d/dispatch rt {:uri "/" :request-method :get})
-; (d/dispatch rt {:uri "/abc" :request-method :get})
+  (let [routes-tree (create-routes-tree routes)]
+    (is (= (dispatch routes-tree {:uri "/" :request-method :get}) {:body :h1}))
+    (is (= (dispatch routes-tree {:uri "/hello" :request-method :get :params {:arg "arg-h2"}}) {:body {:h2 "arg-h2"}}))
+    (is (= (dispatch routes-tree {:uri "/hello/world" :request-method :get}) {:body :h3}))
+    (is (= (dispatch routes-tree {:uri "/abc" :request-method :get}) {:body :404}))))
--- test/paths/test/matcher.clj
+++ test/paths/test/matcher.clj
@@ -1,9 +1,17 @@
 (ns paths.test.matcher
   (:require [clojure.test :refer [deftest testing is]]
+            [mx.paths.tree :refer [create-routes-tree]]
             [mx.paths.matcher :refer :all]))
 
-; matcher.clj
-; (def request {...})
-; (match routes-tree request)
+(def routes [
+  "/has/:route/params/:here" {:get :r1}
+])
+(def routes-tree (create-routes-tree routes))
+
 (deftest matcher
-  )
+  (is (= (match routes-tree {:uri "/has/one/params/two" :request-method :get})
+         [:r1 {:here "two", :route "one"}]))
+  (is (= (match routes-tree {:uri "/has/params/two" :request-method :get})
+         [nil {:route "params"}]))
+  (is (= (match routes-tree {:uri "/ha/params/two" :request-method :get})
+         [nil {}])))
